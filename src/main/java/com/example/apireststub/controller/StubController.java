db6bb903069c0e1d73ff181a2aa95c0ecb4b8183
Kirill Chausov <kirill.chausov2@gmail.com>
2025-09-03 16:07:10 +0300

    feat: refactor AuthResponse and add memory leak
    - Add Lombok dependency
    - refactor AuthResponse (remove default constructor, use @JsonFormat for date serialization)
    - Add /leak endpoint to simulate memory leak via byte array allocation (based on system property 'leak.size.bytes')

--- src/main/java/com/example/apireststub/controller/StubController.java
+++ src/main/java/com/example/apireststub/controller/StubController.java
@@ -5,16 +5,29 @@ import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 @RestController
 @RequestMapping("/api")
 public class StubController {
 
+    private static final List<byte[]> memoryLeak = new ArrayList<>();
+
     @GetMapping("/status")
     public ResponseEntity<String> getSuccessStatus() {
         simulateLittleDelay();
         return ResponseEntity.ok("{\"login\":\"Login1\",\"status\":\"ok\"}");
+    }
+
+    @GetMapping("/leak")
+    public ResponseEntity<String> createMemoryLeak() {
+        simulateLittleDelay();
+        int bytesToLeak = Integer.parseInt(System.getProperty("leak.size.bytes", "0"));
+        memoryLeak.add(new byte[bytesToLeak]);
+        int totalLeakedKB = (int) Math.ceil(memoryLeak.size() * bytesToLeak / 1024.0);
+        return ResponseEntity.ok("{\"totalLeakedKB\":" + totalLeakedKB + "}");
     }
 
     @PostMapping("/login")
--- src/main/java/com/example/apireststub/dto/AuthResponse.java
+++ src/main/java/com/example/apireststub/dto/AuthResponse.java
@@ -1,14 +1,19 @@
 package com.example.apireststub.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthResponse {
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     @NotBlank(message = "Login in required")
     @Size(min=6, max=25, message = "Login must be between 3 and 25 character")
@@ -22,48 +27,6 @@ public class AuthResponse {
             message = "Password must be 6-30 characters without spaces and contain at least one letter and one digit")
     private String password;
 
-    private String date;
-
-    public AuthResponse(String login, String password) {
-        this.login = login;
-        this.password = password;
-        this.date = LocalDateTime.now().format(DATE_TIME_FORMATTER);
-    }
-
-    public AuthResponse() {
-        this.date = LocalDateTime.now().format(DATE_TIME_FORMATTER);
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    @Override
-    public String toString() {
-        return "AuthResponse{" +
-                "login='" + login + '\'' +
-                ", password='" + password + '\'' +
-                ", date='" + date + '\'' +
-                '}';
-    }
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime date = LocalDateTime.now();
 }
